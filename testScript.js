4d2e92108f3c4ea1e24147927763d82600a0aa6a
partychickenking <partychickenking@gmail.com>
2021-04-15 10:30:27 +0200

    added accesiblity colors

--- testScript.js
+++ testScript.js
@@ -1,18 +1,58 @@
+//Change colors
+let root = document.documentElement;
+const blue = document.getElementById('blue')
+const red = document.getElementById('red')
+const yellow = document.getElementById('yellow')
+const lightBlue = document.getElementById('light-blue')
+const black = document.getElementById('black')
+const white = document.getElementById('white')
+
+blue.addEventListener('click', () => {
+    root.style.setProperty('--background', "#112e51")
+})
+
+red.addEventListener('click', () => {
+    root.style.setProperty('--background', "#cd2026")
+})
+
+yellow.addEventListener('click', () => {
+    root.style.setProperty('--background', "#fdb81e")
+})
+
+lightBlue.addEventListener('click', () => {
+    root.style.setProperty('--background', "#02bfe7")
+})
+
+let currentTextColor = getComputedStyle(document.documentElement).getPropertyValue('--text')
+
+black.addEventListener('click', () => {
+    root.style.setProperty('--text', "black")
+})
+
+white.addEventListener('click', () => {
+    root.style.setProperty('--text', "white")
+})
+
+console.log(currentTextColor)
+//Font-size input
 const inputH1 = document.getElementById('input-h1')
 const inputH2 = document.getElementById('input-h2')
 const inputP1 = document.getElementById('input-p1')
 const inputP2 = document.getElementById('input-p2')
 
+//Declare text
 const h1 = document.querySelector('h1');
 const h2 = document.querySelector('h2');
 const p1 = document.querySelector('.p1');
 const p2 = document.querySelector('.p2');
 
+//Add eventlisteners
 inputH1.addEventListener('click', increaseh1)
 inputH2.addEventListener('click', increaseh2)
 inputP1.addEventListener('click', increasep1)
 inputP2.addEventListener('click', increasep2)
 
+//Functions
 function increaseh1() {
     style = window.getComputedStyle(h1, null).getPropertyValue('font-size');
     currentSize = parseFloat(style);
@@ -36,4 +76,3 @@ function increasep2() {
     currentSize = parseFloat(style);
     p2.style.fontSize = (currentSize + 4) + 'px';
 }
-console.log('Hello world!')

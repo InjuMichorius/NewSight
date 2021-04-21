9f0b65ea1d58b33d14693f4b9788e6922cefeb0b
partychickenking <partychickenking@gmail.com>
2021-04-21 14:01:54 +0200

    Added index with content

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-// app.get('/', (req, res) => {
-//     res.render('index')
-// })
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 app.get('/newsight', renderNewSight)
 
